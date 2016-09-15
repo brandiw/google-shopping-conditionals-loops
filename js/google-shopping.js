8e7b54d2424f083523c8fb23f40565d163b3938f
brandiw <brandimichelle.0911@gmail.com>
2016-09-14 21:24:13 -0700

    answer to first three questions

--- js/google-shopping.js
+++ js/google-shopping.js
@@ -1,5 +1,29 @@
 var data = require('../products.json');
 
-// Write your solutions below
+//QUESTION 1
+// var count = 0;
 
-console.log(data.items);
+// for(var i = 0; i < data.items.length; i++){
+//   if(data.items[i].kind == "shopping#product"){
+//     console.log(data.items[i].product.title);
+//     count++;
+//   }
+// }
+
+// console.log("TOTAL ITEMS FOUND: ", count);
+// console.log("LOOKS LIKE WE ALSO HAVE THIS DATA STORED IN 'currentItemCount'");
+
+//QUESTION 2
+// for(var i = 0; i < data.items.length; i++){
+//   if(data.items[i].product.inventories[0].availability == "backorder"){
+//     console.log(data.items[i].product.title, "is currently on back order. :( :( :(");
+//   }
+// }
+
+//QUESTION 3
+for(var i = 0; i < data.items.length; i++){
+  if(data.items[i].product.images.length > 1){
+    console.log("LOOK AT ALL THE PICS OF", data.items[i].product.title);
+  }
+}
+
